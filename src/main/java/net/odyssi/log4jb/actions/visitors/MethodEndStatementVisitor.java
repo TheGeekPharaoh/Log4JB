f6faae56985d11a0781564f6de21b9e7b9f357f9
Steven D. Nakhla <sdnakhla@gmail.com>
2025-01-10 17:02:27 -0500

    Implemented a rudimentary Visitor to add method end log statements

---
+++ src/main/java/net/odyssi/log4jb/actions/visitors/MethodEndStatementVisitor.java
@@ -0,0 +1,59 @@
+package net.odyssi.log4jb.actions.visitors;
+
+import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import org.apache.commons.lang.ArrayUtils;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A {@link org.intellij.markdown.ast.visitors.Visitor} implementation that adds a logging statement before each return
+ * statement in a method
+ */
+public class MethodEndStatementVisitor extends JavaRecursiveElementVisitor {
+
+	private final PsiMethod method;
+
+	public MethodEndStatementVisitor(PsiMethod method) {
+		this.method = method;
+	}
+
+	@Override
+	public void visitReturnStatement(PsiReturnStatement returnStatement) {
+		super.visitReturnStatement(returnStatement);
+		this.addMethodReturnLoggingStatement(returnStatement);
+	}
+
+	@Override
+	public void visitCodeBlock(@NotNull PsiCodeBlock block) {
+		super.visitCodeBlock(block);
+		if (block.getStatements().length == 0 || !(block.getStatements()[block.getStatements().length - 1] instanceof PsiReturnStatement)) {
+			addMethodEndLoggingStatement(block);
+		}
+	}
+
+	private PsiStatement buildLogStatement() {
+		// TODO Generate proper log statement
+		PsiStatement logStatement = JavaPsiFacade.getElementFactory(method.getProject()).createStatementFromText("System.out.println(\"Method " + method.getName() + " returned\");", method);
+		return logStatement;
+	}
+
+	/**
+	 * Adds the method end logging statement immediately before a return statement
+	 *
+	 * @param returnStatement The return statement
+	 */
+	private void addMethodReturnLoggingStatement(PsiReturnStatement returnStatement) {
+		PsiStatement logStatement = buildLogStatement();
+		PsiCodeBlock codeBlock = (PsiCodeBlock) returnStatement.getParent();
+
+		int index = ArrayUtils.indexOf(codeBlock.getStatements(), returnStatement);
+		codeBlock.addBefore(logStatement, codeBlock.getStatements()[index]);
+		CodeStyleManager.getInstance(method.getProject()).reformat(logStatement);
+	}
+
+	private void addMethodEndLoggingStatement(PsiCodeBlock block) {
+		PsiStatement logStatement = this.buildLogStatement();
+		block.add(logStatement);
+		CodeStyleManager.getInstance(method.getProject()).reformat(logStatement);
+	}
+}
