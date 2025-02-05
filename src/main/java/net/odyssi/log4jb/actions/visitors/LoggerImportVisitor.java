9d945d8dc7a7465ce50793a97331aeed3cd96132
Steven D. Nakhla <sdnakhla@gmail.com>
2025-02-05 14:41:35 -0500

    Corrected issue with imports being duplicated

--- src/main/java/net/odyssi/log4jb/actions/visitors/LoggerImportVisitor.java
+++ src/main/java/net/odyssi/log4jb/actions/visitors/LoggerImportVisitor.java
@@ -2,7 +2,9 @@ package net.odyssi.log4jb.actions.visitors;
 
 import com.intellij.psi.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * A {@link org.intellij.markdown.ast.visitors.Visitor} used to import the logger class in the selected class
@@ -14,6 +16,8 @@ public class LoggerImportVisitor extends JavaRecursiveElementVisitor {
 
 	private final List<String> importStatements;
 
+	private final Set<String> existingImports = new HashSet<>();
+
 	public LoggerImportVisitor(PsiJavaFile file, List<String> importStatements) {
 		this.file = file;
 		this.importStatements = importStatements;
@@ -22,14 +26,16 @@ public class LoggerImportVisitor extends JavaRecursiveElementVisitor {
 	@Override
 	public void visitImportStatement(PsiImportStatement element) {
 		super.visitImportStatement(element);
-		importStatements.remove(element.getImportReference().getQualifiedName());
+		existingImports.add(element.getImportReference().getQualifiedName());
 	}
 
 	@Override
 	public void visitFile(PsiFile file) {
 		super.visitFile(file);
 		for (String importStatement : importStatements) {
-			addImportStatement(importStatement);
+			if (!existingImports.contains(importStatement)) {
+				addImportStatement(importStatement);
+			}
 		}
 	}
 
