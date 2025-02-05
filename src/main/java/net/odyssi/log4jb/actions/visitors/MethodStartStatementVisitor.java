4acea8b58596fa9eccd5b17291085f7853a5134d
Steven D. Nakhla <sdnakhla@gmail.com>
2025-02-05 14:27:30 -0500

    Added checks for existing start methods

--- src/main/java/net/odyssi/log4jb/actions/visitors/MethodStartStatementVisitor.java
+++ src/main/java/net/odyssi/log4jb/actions/visitors/MethodStartStatementVisitor.java
@@ -1,19 +1,20 @@
 package net.odyssi.log4jb.actions.visitors;
 
-import com.intellij.psi.*;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiStatement;
 import com.intellij.psi.codeStyle.CodeStyleManager;
 
 /**
  * A {@link org.intellij.markdown.ast.visitors.Visitor} implementation that can be used to add a logging statement
  * to the beginning of the selected method
  */
-public class MethodStartStatementVisitor extends AbstractMethodLoggingVisitor{
-
-	private static final String guardedLogStatementTemplate = "if(%s.isDebugEnabled()) {\n	%s.debug(\"%s\");\n}";
-
-	private static final String logStatementStart = " - start";
+public class MethodStartStatementVisitor extends AbstractMethodLoggingVisitor {
 
 	public static final String loggerObjectName = "logger";
+	private static final String guardedLogStatementTemplate = "if(%s.isDebugEnabled()) {\n	%s.debug(\"%s\");\n}";
+	private static final String logStatementStart = " - start";
 
 	public MethodStartStatementVisitor(PsiMethod method) {
 		super(method);
@@ -21,22 +22,35 @@ public class MethodStartStatementVisitor extends AbstractMethodLoggingVisitor{
 
 	@Override
 	public void visitMethod(PsiMethod method) {
-		if(method.isConstructor()) {
+		if (method.isConstructor()) {
 			return;
 		}
 
 		super.visitMethod(method);
 		if (method.getBody() != null) {
 			PsiStatement logStatement = buildLogStatement();
+			String logStatementText = logStatement.getText().replaceAll("\\s+", "");
 			PsiStatement[] statements = method.getBody().getStatements();
-			if (statements.length > 0) {
-				// TODO Add check for existing log statements
-				method.getBody().addBefore(logStatement, statements[0]);
-			} else {
-				// TODO Add check for existing log statements
-				method.getBody().add(logStatement);
+
+			// Check if a matching log statement already exists
+			boolean logStatementExists = false;
+			for (PsiStatement statement : statements) {
+				String statementText = statement.getText().replaceAll("\\s+", "");
+				boolean match = logStatementText.equals(statementText);
+				if (match) {
+					logStatementExists = true;
+					break;
+				}
 			}
-			CodeStyleManager.getInstance(method.getProject()).reformat(logStatement);
+
+			if (!logStatementExists) {
+				if (statements.length > 0) {
+					method.getBody().addBefore(logStatement, statements[0]);
+				} else {
+					method.getBody().add(logStatement);
+				}
+				CodeStyleManager.getInstance(method.getProject()).reformat(logStatement);
+			}
 		}
 	}
 
