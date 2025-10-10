026976a746fe67552b6101ad8d035532eac55f32
Steven D. Nakhla <sdnakhla@gmail.com>
2025-10-10 09:42:41 -0400

    Added exception logging

--- src/main/java/net/odyssi/log4jb/visitors/LogMethodVisitor.java
+++ src/main/java/net/odyssi/log4jb/visitors/LogMethodVisitor.java
@@ -7,24 +7,26 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 /**
- * A PSI visitor that instruments a Java method with start and end log statements.
+ * A PSI visitor that instruments a Java method with start/end and exception log statements.
  * <p>
- * This visitor adds guarded log statements at the debug level.
- * - A "start" log statement is added at the beginning of the method body.
- * - An "end" log statement is added before every return statement and at the end of the method body.
+ * This visitor adds:
+ * - Guarded "start" and "end" log statements at the debug level.
+ * - "warn" or "error" log statements to all catch blocks.
  * <p>
  * It avoids adding duplicate log statements if they already appear to exist.
  */
 public class LogMethodVisitor extends JavaRecursiveElementVisitor {
 
     private final PsiMethod psiMethod;
+    private final String methodSignature;
     private final String startMessage;
     private final String endMessage;
 
     public LogMethodVisitor(PsiMethod psiMethod) {
         this.psiMethod = psiMethod;
-        this.startMessage = buildMessage(psiMethod, "start");
-        this.endMessage = buildMessage(psiMethod, "end");
+        this.methodSignature = buildMethodSignature(psiMethod);
+        this.startMessage = String.format("%s - start", this.methodSignature);
+        this.endMessage = String.format("%s - end", this.methodSignature);
     }
 
     @Override
@@ -46,31 +48,59 @@ public class LogMethodVisitor extends JavaRecursiveElementVisitor {
         }
 
         // 2. Add "end" log statements before all return statements
-        // We only need to check for the end message once, as we will add it to all returns if it's missing anywhere.
         if (!bodyText.contains(endMessage)) {
             final var returnStatements = PsiTreeUtil.findChildrenOfType(body, PsiReturnStatement.class);
             for (PsiReturnStatement returnStatement : returnStatements) {
                 final var endLogStatementText = String.format("if(logger.isDebugEnabled()) { logger.debug(\"%s\"); }", endMessage);
                 final var endLogStatement = factory.createStatementFromText(endLogStatementText, returnStatement);
-                // Use addBefore to place the log statement just before the return
                 returnStatement.getParent().addBefore(endLogStatement, returnStatement);
             }
         }
 
         // 3. Add "end" log statement at the very end of the method if it can "fall through"
-        // (i.e., it doesn't end with a return or throw)
         final var statements = body.getStatements();
         if (statements.length == 0) {
-            // Handle empty method body
             if (!body.getText().contains(endMessage)) {
                 addFinalEndLog(body, factory);
             }
         } else {
             final var lastStatement = statements[statements.length - 1];
-            // If the last statement is not a return, we need a final log statement.
-            // Also check if the log message is already present to avoid duplicates.
             if (!(lastStatement instanceof PsiReturnStatement) && !lastStatement.getText().contains(endMessage)) {
                 addFinalEndLog(body, factory);
+            }
+        }
+
+        // 4. Add logging to all catch blocks.
+        instrumentCatchBlocks(method, factory);
+    }
+
+    private void instrumentCatchBlocks(PsiMethod method, PsiElementFactory factory) {
+        for (PsiCatchSection catchSection : PsiTreeUtil.findChildrenOfType(method, PsiCatchSection.class)) {
+            final PsiCodeBlock catchBlock = catchSection.getCatchBlock();
+            final PsiParameter exceptionParameter = catchSection.getParameter();
+
+            if (catchBlock == null || exceptionParameter == null) {
+                continue;
+            }
+
+            final String exceptionName = exceptionParameter.getName();
+            final String logStatementText;
+
+            // Check if the catch block is empty (contains no statements).
+            if (catchBlock.getStatementCount() == 0) {
+                // Empty block: log a warning that the exception is ignored.
+                final String message = String.format("%s - exception ignored", this.methodSignature);
+                logStatementText = String.format("logger.warn(\"%s\", %s);", message, exceptionName);
+            } else {
+                // Block has statements: log an error.
+                logStatementText = String.format("logger.error(\"%s\", %s);", this.methodSignature, exceptionName);
+            }
+
+            // Avoid adding a duplicate log statement.
+            if (!catchBlock.getText().contains(logStatementText)) {
+                final PsiStatement logStatement = factory.createStatementFromText(logStatementText, catchBlock);
+                // Add the log statement as the first line inside the catch block.
+                catchBlock.addAfter(logStatement, catchBlock.getLBrace());
             }
         }
     }
@@ -85,16 +115,16 @@ public class LogMethodVisitor extends JavaRecursiveElementVisitor {
     }
 
     /**
-     * Builds the log message string.
-     * Example: "myMethod(String,int,List) - start"
+     * Builds the method signature part of the log message.
+     * Example: "myMethod(String,int,List)"
      */
-    private String buildMessage(PsiMethod method, String suffix) {
+    private String buildMethodSignature(PsiMethod method) {
         final var methodName = method.getName();
         final var parameterTypes = Arrays.stream(method.getParameterList().getParameters())
                 .map(PsiParameter::getType)
                 .map(PsiType::getPresentableText)
                 .collect(Collectors.joining(","));
 
-        return String.format("%s(%s) - %s", methodName, parameterTypes, suffix);
+        return String.format("%s(%s)", methodName, parameterTypes);
     }
 }
